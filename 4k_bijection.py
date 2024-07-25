18ce2e197b5c0bfcd419e10d15ad75e93c12e2f4
indi <indivicivet@gmail.com>
2024-07-26 00:15:35 +0100

    fix types and do RGBCMY while we're at it (still result-violating)

--- 4k_bijection.py
+++ 4k_bijection.py
@@ -7,27 +7,33 @@ im_path = (
     Path(__file__).parent
     / "easy-peasy-dot-ai__fc61fa66-850f-48d0-b287-2131be2d8880.png"
 )
-im = np.array(Image.open(im_path).convert("RGB").resize((4096, 4096)))
+im = np.array(Image.open(im_path).convert("RGB").resize((4096, 4096)), dtype=float)
 im_1d_col = im.reshape(4096 * 4096, 3)
-sorted_1dc = np.array([
+sorted_1dc_lookup = np.array([
     (r, g, b)
     for r in range(256)
     for g in range(256)
     for b in range(256)
 ], dtype=np.uint8).reshape(4096 * 4096, 3)
+sorted_1dc_values = sorted_1dc_lookup.astype(float)
 
 
-def distances(i1d, axis):
-    return i1d[..., axis] / (1e-5 + np.sum(i1d ** 2, axis=1) ** 0.5)
+def distances(i1d, axs):
+    return (
+        np.sum(i1d[..., axs] ** 2, axis=1) ** 0.5
+        / (1e-5 + np.sum(i1d ** 2, axis=1) ** 0.5)
+    )
 
 
 PER_COL_N = 4096 * 4096 // 10
 
-remapped = np.zeros_like(sorted_1dc)
-for ax in [1, 0, 2]:
+remapped = np.zeros_like(sorted_1dc_lookup)
+for axes in [[1], [0], [2], [0, 1], [1, 2], [0, 2]]:
     remapped[
-        np.argsort(distances(im_1d_col, ax))[-PER_COL_N:]
-    ] = sorted_1dc[np.argsort(distances(sorted_1dc, ax))[-PER_COL_N:]]
+        np.argsort(distances(im_1d_col, axes))[-PER_COL_N:]
+    ] = sorted_1dc_lookup[
+        np.argsort(distances(sorted_1dc_values, axes))[-PER_COL_N:]
+    ]
 
 result_im = Image.fromarray(remapped.reshape((4096, 4096, 3)))
 result_im.show()
