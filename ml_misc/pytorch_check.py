1c8f56711e97e83592fc32ebc0705b4cc0df0198
indi <indivicivet@gmail.com>
2025-08-05 23:18:40 +0100

    add pytorch check stub example for tlm

---
+++ ml_misc/pytorch_check.py
@@ -0,0 +1,16 @@
+import torch
+
+
+class PlusMinusModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.param = torch.nn.Parameter(torch.tensor(10.0))
+
+    def forward(self, x):
+        return (self.param ** 2 * x + (-self.param)).sum()                                  # scalar loss
+
+
+model = PlusMinusModule()
+loss = model(torch.ones(5))
+loss.backward()
+print(f"{loss.item()=}, {model.param=}, {model.param.grad=}")
