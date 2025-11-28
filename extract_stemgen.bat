57d4cde03d5f52a076dcab2cbc657b3f372607da
indi <indivicivet@gmail.com>
2025-11-28 23:59:36 +0000

    another linebreak

--- extract_stemgen.bat
+++ extract_stemgen.bat
@@ -3,6 +3,7 @@
 set "FILENAME=%~1"
 
 if not exist ".\temp" mkdir ".\temp"
+
 ffmpeg -i "D:\music\%FILENAME%.mp3" ".\temp\%FILENAME%.wav"
 
 call "D:\music_prod\demucs_py310_v2\Scripts\python.exe" "D:\repos\stemgen\stemgen.py" --i ".\temp\%FILENAME%.wav" -f aac -o "D:\music_prod\stems_openfmt"
