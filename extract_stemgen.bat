d3b9119eec16632c2db389dc0bc0b39ab037bb84
indi <indivicivet@gmail.com>
2025-11-28 23:46:48 +0000

    one line call is better ^^' (no '"' error)

--- extract_stemgen.bat
+++ extract_stemgen.bat
@@ -3,7 +3,6 @@
 set "FILENAME=%~1"
 if not exist ".\temp" mkdir ".\temp"
 ffmpeg -i "D:\music\%FILENAME%.mp3" ".\temp\%FILENAME%.wav"
-call "D:\music_prod\demucs_py310_v2\Scripts\python.exe" ^
-  "D:\repos\stemgen\stemgen.py" --i ".\temp\%FILENAME%.wav" -f aac -o "D:\music_prod\stems_openfmt"
+call "D:\music_prod\demucs_py310_v2\Scripts\python.exe" "D:\repos\stemgen\stemgen.py" --i ".\temp\%FILENAME%.wav" -f aac -o "D:\music_prod\stems_openfmt"
 del ".\temp\%FILENAME%.wav"
 powershell -c "[console]::beep(1000,500)"
