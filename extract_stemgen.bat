0741e9413af3526a070842aa986ec2e50c34bb0a
indi <indivicivet@gmail.com>
2025-11-29 01:25:34 +0000

    bring back htdemucs

--- extract_stemgen.bat
+++ extract_stemgen.bat
@@ -6,7 +6,7 @@ if not exist ".\temp" mkdir ".\temp"
 
 ffmpeg -i "D:\music\%FILENAME%.mp3" ".\temp\%FILENAME%.wav"
 
-call "D:\music_prod\demucs_py310_v2\Scripts\python.exe" "D:\repos\stemgen\stemgen.py" --i ".\temp\%FILENAME%.wav" -f aac -o "D:\music_prod\stems_openfmt"
+call "D:\music_prod\demucs_py310_v2\Scripts\python.exe" "D:\repos\stemgen\stemgen.py" --i ".\temp\%FILENAME%.wav" -n htdemucs -f aac -o "D:\music_prod\stems_openfmt"
 
 echo "you should delete .\temp\%FILENAME%.wav"
 
