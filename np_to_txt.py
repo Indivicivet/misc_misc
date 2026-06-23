486209be6f57963352deac5bca071db0749bcf9f
indi <indivicivet@gmail.com>
2026-06-23 13:49:04 +0100

    fix typos etc

--- np_to_txt.py
+++ np_to_txt.py
@@ -1,11 +1,11 @@
 """
 .npy to text intended for diffing .npy files
 
-git config --global diff.np_to_txt.textconv "py <path to>\np_to_txt.py"
+git config --global diff.np_to_txt.textconv "py <path to>/np_to_txt.py"
 
 add to gitattributes:
 *.npy diff=np_to_txt
-e.g.
+e.g. windows default:
 %USERPROFILE%\.config\git\attributes
 or get path via
 git config --global core.attributesFile
@@ -17,7 +17,7 @@ import numpy as np
 
 
 def print_arr(arr):
-    print(f"shape {arr.shape}"
+    print(f"shape {arr.shape}")
     print(f"dtype {arr.dtype}")
     with np.printoptions(threshold=10_000, linewidth=120)
         print(arr)
